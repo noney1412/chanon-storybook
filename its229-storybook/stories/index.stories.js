ed75d341812c872501d0d1c0844a70098b519cc1
noney1412 <nonpanpila@gmail.com>
2018-02-20 23:32:57 +0700

    :memo: add storybook

---
+++ its229-storybook/stories/index.stories.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+
+import { Button, Welcome } from "@storybook/react/demo";
+
+import Nav from "../components/nav";
+
+storiesOf("Welcome", module).add("to Storybook", () => (
+	<Welcome showApp={linkTo("Button")} />
+));
+
+storiesOf("Button", module)
+	.add("with text", () => (
+		<Button onClick={action("clicked")}>Hello Button</Button>
+	))
+	.add("with some emoji", () => (
+		<Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
+	))
+	.add("navbar", () => <Nav />);
